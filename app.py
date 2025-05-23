e486d89e35407917e499dbf6c37a09c5df7afc4f
ctorres4564 <ctorres4564@gmail.com>
2025-05-23 20:26:47 -0300

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,19 @@
+print("""𝑺𝒂𝒃𝒐𝒓 𝑬𝒙𝒑𝒓𝒆𝒔𝒔
+      """)
+
+print("1. Cadastrar Restaurante")
+print("2. Listar Restaurantes")
+print("3. Ativar Restaurante")
+print("4. Sair\n")
+
+opcao_escolhida = input("Escolha uma opção: ")
+print("Você escolheu a opção: ", opcao_escolhida)
+
+if opcao_escolhida == "1":
+    print("Cadastrar Restaurante")
+elif opcao_escolhida == "2":
+    print("Listar Restaurantes")
+elif opcao_escolhida == "3":
+    print("Ativar Restaurante")
+elif opcao_escolhida == "4":
+    print("Sair")
